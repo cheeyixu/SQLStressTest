3ff7e35ccff2e048a05bd397146769da58f73294
CHEE0109 <CHEE0109@ERIANNB18>
2016-10-20 14:08:16 +0800

    Add project files.

---
+++ SQLStressTest/BankDBTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLStressTest
+{
+    class BankDBTest
+    {
+        static void Main(string[] args)
+        {
+            string connetionString = null;
+            SqlConnection cnn;
+            connetionString = "Data Source=SQL-SRV-2016\\SQLSRV2016;Initial Catalog=BankTestDB;User ID=sa;Password=saPwd123";
+            cnn = new SqlConnection(connetionString);
+            try
+            {
+                cnn.Open();
+                Console.WriteLine("Connection Open ! ");
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Can not open connection ! " + ex.ToString());
+            }
+        }
+    }
+}
